ddf50a660cbb0752f22fd2ea694de8fb078e1df1
pravasgurung476 <154384648+pravasgurung476@users.noreply.github.com>
2025-06-21 00:55:32 +0545

    added categories component

--- src/components/Category/Categories.js
+++ src/components/Category/Categories.js
@@ -5,10 +5,20 @@ import categories from './CategoryData';
 
 function Categories() {
   return (
-    <div style={{ display: 'flex', gap: '20px', flexWrap: 'wrap', justifyContent: 'center' }}>
-      {categories.map((cat, index) => (
-        <CategoryBox key={index} icon={cat.icon} label={cat.label} />
-      ))}
+    <div>
+      <div style={{ textAlign: '', margin: '20px 0px 0px 30px' }}>
+        <h5 className="BaseFont">
+          Categories
+        </h5>
+        <h2 className='BaseFont'>
+          Browse by categories
+        </h2>
+      </div>
+        <div style={{ display: 'flex', gap: '20px', flexWrap: 'wrap', justifyContent: 'center' }}>
+          {categories.map((cat, index) => (
+            <CategoryBox key={index} icon={cat.icon} label={cat.label} />
+          ))}
+        </div>
     </div>
   );
 }
