861293c70050817c39e3d42a372bfe98768d9063
pravasgurung476 <154384648+pravasgurung476@users.noreply.github.com>
2025-06-21 18:32:12 +0545

    first commit fo products

---
+++ src/components/Products/ProductData.js
@@ -0,0 +1,23 @@
+import esp32 from '../../assets/ProductsImages/esp32.jpg';
+import arduinouno from '../../assets/ProductsImages/arduinouno.jpg';
+import tempsensor from '../../assets/ProductsImages/tempsensor.jpg';
+
+const products = [
+  {
+    image: esp32,
+    name: 'ESP32 Development Board',
+    price: '299.99',
+  },
+  {
+    image: arduinouno,
+    name: 'Arduino UNO R3',
+    price: '499.99',
+  },
+  {
+    image: tempsensor,
+    name: 'DHT11 Temperature Sensor',
+    price: '199.99',
+  },
+];
+
+export default products;
